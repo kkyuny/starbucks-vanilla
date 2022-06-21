2e06316687b8f50292f8203aec6b94fd0c1a50d2
kyun <88278485+kkyuny@users.noreply.github.com>
2022-06-21 16:13:47 +0900

    ~13강

--- js/main.js
+++ js/main.js
@@ -15,8 +15,25 @@ searchInputEl.addEventListener('blur', function() {
   searchInputEl.setAttribute('placeholder', '');
 });
 
-const badgeEl = document.querySelector('header.badges');
+const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', function() {
-  console.log('scroll!');
-});
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500){
+    // 배지 숨기기
+    //badgeEl.style.display = 'none';
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // 배지 보이기
+    //badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+},300));
+// _.throttle(함수,시간) : 함수를 시간 단위마다 실행하도록 한다.
